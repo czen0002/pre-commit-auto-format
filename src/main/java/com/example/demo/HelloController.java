6c58ca3066344f13b6af9e870d5b17d3abc3f7f3
czen0002 <zc114789@gmail.com>
2026-02-03 23:21:47 +1100

    initial

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class HelloController {
+
+  @GetMapping("/hello")
+  String hello() {
+
+
+
+    return "Hello Spring Java" +
+        "" +
+        " Format!";
+  }
+
+}
