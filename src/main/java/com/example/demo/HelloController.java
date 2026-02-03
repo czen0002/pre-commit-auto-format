4c3a508419d3bce029638c439fd7b1203970eadf
czen0002 <zc114789@gmail.com>
2026-02-03 23:51:36 +1100

    always run

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -6,14 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 class HelloController {
 
-  @GetMapping("/hello")
-  String hello() {
+	@GetMapping("/hello")
+	String hello() {
 
-
-
-    return "Hello Spring Java" +
-        "" +
-        " Format!";
-  }
+		return "Hello Spring Java Format!";
+	}
 
 }
